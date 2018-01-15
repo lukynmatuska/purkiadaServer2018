1743a963c56b76660c0942fa9572dca9e428d0b7
lukynmatuska <lukynmatuska@gmail.com>
2018-01-15 15:10:36 +0100

    Panel added
    Successfully working

--- purkiadaServePanel.py
+++ purkiadaServePanel.py
@@ -9,7 +9,7 @@ class HtmlPage():
         self.title = "{} - PurkiadaServer".format(title)
         self.menuButtonName = title
 
-        self.menuList = ['Home', 'About', 'Contact', "Login", "Register"]
+        self.menuList = ['Home', 'About', 'Contact', "Login", "Register", "Status"]
         if not self.menuButtonName in self.menuList:
             self.menuList.append(self.menuButtonName)
         self.content =  content
@@ -40,6 +40,8 @@ class HtmlPage():
                     hr()
                     if type(self.content) == str:
                         p(self.content)
+                        div(self.content)
+                        
                     with div(cls="paticka", style="text-alig: center;"):
                         hr()
                         with p("Copyright &copy; 2018, ", style="text-align: center; font-size: 75%; border=0%; padding=0%"):
@@ -73,28 +75,61 @@ class HtmlPage():
                 self.f2 = open("C:\\xampp\\htdocs\\purkiadaServer2018\\{}.html".format(self.htmlFileName), "w")
                 self.f2.write(str(self.update()))
                 self.f2.close()
-                print("\nFILE NAME: C:\\xampp\\htdocs\\purkiadaServer2018\\{}.html".format(self.htmlFileName))
+                print("FILE NAME: C:\\xampp\\htdocs\\purkiadaServer2018\\{}.html".format(self.htmlFileName))
                 break
             except:
                 print("Error with saving html file! to C:\\xampp\\htdocs\\purkiadaServer2018\\")
         
         
 home = HtmlPage("Home", "Ahoj svete! z Homu", "Home")
+status = HtmlPage("Status", "Ahoj svete! ze Statusu", "Status")
 contact = HtmlPage("Contact", "Ahoj svete! z contact", "Contact")
 about = HtmlPage("About", "Ahoj svete! z about", "About")
 login = HtmlPage("Login", "Ahoj svete! z login", "Login")
 register = HtmlPage("Register", "Ahoj svete! z Register", "Register")
 print(".: Websites creation SUCCES COMPLETED! :.")
 
-import sys
-import socket
 import threading
 import time
-cThread = threading.Thread(target=home.update, args=())
-cThread.daemon = True
-cThread.start()
-while True:
-    time.sleep(5)
-    print("Running...")
-    
-print("Stoppped")
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(threadName)-10s] %(message)s',
+                    )
+def saveIt():
+    while True:
+        time.sleep(5)
+        #home.save()
+        status.save()
+        #print("Saved at {}".format(time.time()))
+        logging.debug("Saved at {}".format(time.asctime( time.localtime(time.time()) )))
+
+def daemon():
+    logging.debug('Running')
+    saveIt()
+    while True:
+        #home.update()
+        status.update()
+        #print("updated at {}".format(time.time()))
+        logging.debug("Updated at {}".format(time.asctime( time.localtime(time.time()) )))
+
+#time.sleep(2)
+def deamonStop():
+    logging.debug('Exiting')
+
+d = threading.Thread(name='daemon', target=daemon)
+d.setDaemon(True)
+
+def non_daemon():
+    logging.debug('Starting')
+    logging.debug('Exiting')
+
+#t = threading.Thread(name='non-daemon', target=non_daemon)
+
+d.start()
+#t.start()
+
+d.join(1)
+#print 'd(isAlive()', d.isAlive())
+#t.join()    
+#print("Stoppped")
