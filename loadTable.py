8da8db81b69b98e0f495764b5d64e01b05e70295
lukynmatuska <lukynmatuska@gmail.com>
2018-01-15 21:38:10 +0100

    Table load credentials

--- loadTable.py
+++ loadTable.py
@@ -3,16 +3,17 @@ import xlrd
 fileLocation = "table.xlsx"
 workbook = xlrd.open_workbook(fileLocation)
 sheet = workbook.sheet_by_index(0)
-sheet.call_value(0,0)
-print(sheet.nrows)
-print(sheet.ncols)
-"""
-for col in range(sheet.ncols):
-	sheet.cell_value(1, col)#Login
-	sheet.cell_value(2, col)#Heslo"""
-for row in range(sheet.nrows):
-	sheet.cell_value(row, 1)#Login
-	sheet.cell_value(row, 2)#Heslo
-
+#sheet.cell_value(0,0)
+#print(sheet.nrows)
+#print(sheet.ncols)
 data = [[sheet.cell_value(r,c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
 
+users = []
+for i in range(1, len(data)):
+    users.append(data[i][1])
+print("Users: {}\n".format(users))
+
+pswds = []
+for i in range(1, len(data)):
+    pswds.append(data[i][2])
+print("Pswds: {}".format(pswds))
