9368dff8234aa8420f527a5263372f8d5afb1463
lukynmatuska <lukynmatuska@gmail.com>
2018-01-24 13:52:30 +0100

    table & .exe UPDATE

--- loadTable.py
+++ loadTable.py
@@ -1,6 +1,6 @@
 import xlrd
 
-fileLocation = "table.xlsx"
+fileLocation = "c:\\temp\\table.xlsx"
 workbook = xlrd.open_workbook(fileLocation)
 sheet = workbook.sheet_by_index(0)
 #sheet.cell_value(0,0)
@@ -10,10 +10,10 @@ data = [[sheet.cell_value(r,c) for c in range(sheet.ncols)] for r in range(sheet
 
 users = []
 for i in range(1, len(data)):
-    users.append(data[i][1])
+    users.append(data[i][4])#1
 #print("Users: {}\n".format(users))
 
 pswds = []
 for i in range(1, len(data)):
-    pswds.append(data[i][2])
+    pswds.append(data[i][3])#2
 #print("Pswds: {}".format(pswds))
